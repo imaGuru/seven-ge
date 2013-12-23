80029eec7aa25a0dde8521259e2168565cbbb85d
SeventhSon <myselph.tom@gmail.com>
2013-12-23 17:53:06 +0100

    GameEngine class

---
+++ src/com/example/sevenge/GameEngine.java
@@ -0,0 +1,25 @@
+package com.example.sevenge;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+import android.opengl.GLSurfaceView.Renderer;
+
+public class GameEngine implements Renderer {
+
+	@Override
+	public void onDrawFrame(GL10 arg0) {
+		
+	}
+
+	@Override
+	public void onSurfaceChanged(GL10 gl, int width, int height) {
+		
+	}
+
+	@Override
+	public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+		
+	}
+
+}

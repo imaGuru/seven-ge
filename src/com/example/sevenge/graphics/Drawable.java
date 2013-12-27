23113977af7ce3cf1df767a081f5d403e13364b6
SeventhSon <myselph.tom@gmail.com>
2013-12-27 15:49:43 +0100

    Basic classes created. Simple renderer of drawable objects setup

---
+++ src/com/example/sevenge/graphics/Drawable.java
@@ -0,0 +1,5 @@
+package com.example.sevenge.graphics;
+
+public interface Drawable {
+	public void draw();
+}
---
+++ src/com/example/sevenge/graphics/Renderer.java
@@ -0,0 +1,27 @@
+package com.example.sevenge.graphics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Renderer {
+	private List<Drawable> RenderQueue;
+	private int i;
+	Renderer()
+	{
+		RenderQueue = new ArrayList<Drawable>(50);
+	}
+	public void render()
+	{
+		for(i=0;i<RenderQueue.size();i++)
+			RenderQueue.get(i).draw();
+		RenderQueue.clear();
+	}
+	public void addToRender(Drawable d)
+	{
+		RenderQueue.add(d);
+	}
+	public void addToRender(List<Drawable> ds)
+	{
+		RenderQueue.addAll(ds);
+	}
+}

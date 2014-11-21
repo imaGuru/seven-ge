5421b1c6133aa1d7a7341d20a80f8fc460a32482
SeventhSon <myselph.tom@gmail.com>
2014-11-21 21:06:20 +0100

    IO and ShaderProgram tests

--- SevenGETestsTest/src/com/engine/sevenge/test/IOTest.java
+++ SevenGETestsTest/src/com/engine/sevenge/test/IOTest.java
@@ -1,6 +1,9 @@
 
 package com.engine.sevenge.test;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -23,6 +26,8 @@ public class IOTest extends AndroidTestCase {
 	@Override
 	public void tearDown () {
 		SevenGE.io = null;
+		IO.internal("testCopy.pkg").delete();
+		IO.internal("renamed.pkg").delete();
 	}
 
 	public void testOpenAssetsValid () {
@@ -40,6 +45,57 @@ public class IOTest extends AndroidTestCase {
 			IO.openAsset("nonexistent.png");
 			Assert.fail();
 		} catch (IOException e) {
+		}
+	}
+
+	public void testOpenInternalFile () {
+		File file = IO.internal("Scripts/main.lua");
+		assertFalse(file.exists());
+	}
+
+	public void testOpenExternalFile () {
+		File file = IO.external("Scripts/main.lua");
+		assertFalse(file.exists());
+	}
+
+	public void testOpenCacheFile () {
+		File file = IO.cache("Scripts/main.lua");
+		assertFalse(file.exists());
+	}
+
+	public void testReadToString () {
+		try {
+			String s = IO.readToString(IO.openAsset("Scripts/main.lua"));
+			assertFalse(s.isEmpty());
+		} catch (IOException e) {
+			Assert.fail("File not found");
+			e.printStackTrace();
+		}
+	}
+
+	public void testRenameFile () {
+		try {
+			File file1 = IO.internal("test.pkg");
+			file1.createNewFile();
+			File file2 = IO.internal("renamed.pkg");
+			IO.renameFile(file1, file2);
+			assertTrue(file2.isFile() && file2.exists());
+		} catch (IOException e) {
+			e.printStackTrace();
+			Assert.fail("Failed to rename file");
+		}
+	}
+
+	public void testCopyFile () {
+		try {
+			File file1 = IO.internal("test.pkg");
+			file1.createNewFile();
+			File file2 = IO.internal("testCopy.pkg");
+			IO.copyFile(new FileInputStream(file1), new FileOutputStream(file2));
+			assertTrue(file2.isFile() && file2.exists());
+		} catch (IOException e) {
+			e.printStackTrace();
+			Assert.fail("Failed to rename file");
 		}
 	}
 
---
+++ SevenGETestsTest/src/com/engine/sevenge/test/ShaderProgramTest.java
@@ -0,0 +1,66 @@
+
+package com.engine.sevenge.test;
+
+import android.opengl.GLES20;
+
+import com.engine.sevenge.graphics.ColorShaderProgram;
+import com.engine.sevenge.graphics.Shader;
+import com.engine.sevenge.graphics.TextureShaderProgram;
+import com.engine.sevenge.io.IO;
+
+public class ShaderProgramTest extends BaseOpenGLES20UnitTest {
+
+	public ShaderProgramTest () {
+
+	}
+
+	public void testColorProgramLoading () throws Throwable {
+		runOnGLThread(new TestWrapper() {
+			@Override
+			public void executeTest () throws Throwable {
+				Shader v = new Shader(IO.readToString(IO.openAsset("Shaders/simple_vertex_shader.glsl")), GLES20.GL_VERTEX_SHADER);
+				Shader f = new Shader(IO.readToString(IO.openAsset("Shaders/simple_fragment_shader.glsl")), GLES20.GL_FRAGMENT_SHADER);
+				ColorShaderProgram csp = new ColorShaderProgram(v, f);
+				assertFalse(csp.getGLID() == 0);
+			}
+		});
+	}
+
+	public void testTextureProgramLoading () throws Throwable {
+		runOnGLThread(new TestWrapper() {
+			@Override
+			public void executeTest () throws Throwable {
+				Shader v = new Shader(IO.readToString(IO.openAsset("Shaders/texture_vertex_shader.glsl")), GLES20.GL_VERTEX_SHADER);
+				Shader f = new Shader(IO.readToString(IO.openAsset("Shaders/texture_fragment_shader.glsl")),
+					GLES20.GL_FRAGMENT_SHADER);
+				TextureShaderProgram tsp = new TextureShaderProgram(v, f);
+				assertFalse(tsp.getGLID() == 0);
+			}
+		});
+	}
+
+	public void testProgramValidationInvalid () throws Throwable {
+		runOnGLThread(new TestWrapper() {
+			@Override
+			public void executeTest () throws Throwable {
+				Shader f = new Shader(IO.readToString(IO.openAsset("Shaders/texture_fragment_shader.glsl")),
+					GLES20.GL_FRAGMENT_SHADER);
+				TextureShaderProgram tsp = new TextureShaderProgram(f, f);
+				assertFalse(tsp.validateProgram());
+			}
+		});
+	}
+
+	public void testProgramValidationValid () throws Throwable {
+		runOnGLThread(new TestWrapper() {
+			@Override
+			public void executeTest () throws Throwable {
+				Shader v = new Shader(IO.readToString(IO.openAsset("Shaders/texture_vertex_shader.glsl")), GLES20.GL_VERTEX_SHADER);
+				Shader f = new Shader(IO.readToString(IO.openAsset("Shaders/texture_fragment_shader.glsl")),
+					GLES20.GL_FRAGMENT_SHADER);
+				TextureShaderProgram tsp = new TextureShaderProgram(v, f);
+				assertTrue(tsp.validateProgram());
+			}
+		});
+	}
+}

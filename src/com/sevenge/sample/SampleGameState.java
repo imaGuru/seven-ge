cef43b2a1b7cd46ea69b0b060a5cf4836705ee91
kiyoqoko <kuroshiro@ymail.com>
2014-12-06 19:32:39 +0100

    demo

--- src/com/sevenge/sample/SampleGameState.java
+++ src/com/sevenge/sample/SampleGameState.java
@@ -1,4 +1,3 @@
-
 package com.sevenge.sample;
 
 import java.util.ArrayList;
@@ -26,208 +25,273 @@ import com.sevenge.ecs.SpriteComponent;
 import com.sevenge.input.GestureProcessor;
 import com.sevenge.input.InputProcessor;
 import com.sevenge.utils.Log;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.CircleShape;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
 
 public class SampleGameState extends GameState implements InputProcessor, GestureProcessor {
 
-	private final String TAG = "InputTest";
+  private final String TAG = "InputTest";
+  static final float BOX_STEP = 1 / 60f;
+  static final int BOX_VELOCITY_ITERATIONS = 6;
+  static final int BOX_POSITION_ITERATIONS = 2;
+  static final float WORLD_TO_BOX = 0.01f;
+  static final float BOX_WORLD_TO = 100f;
+  
 
-	private Music music;
-	private RendererSystem rendererSystem;
-	private CameraSystem cameraSystem;
-	private AnimationSystem animationSystem;
-	private List<Entity> entities;
-	private int changeme = 0;
-	private Component[] optimizedEntites = new Component[10000];
-	private int oec = 0;
-	private CameraComponent cc;
-	private ScriptingSystem scriptingSystem;
 
-	private float angle;
+  private RendererSystem rendererSystem;
+  private CameraSystem cameraSystem;
+  private List<Entity> entities;
+  private int changeme = 0;
+  private Component[] optimizedEntites = new Component[10000];
+  private int oec = 0;
+  private CameraComponent cc;
+  private ScriptingSystem scriptingSystem;
 
-	private float cameraX;
+  private float angle;
 
-	private float cameraY;
+  private float cameraX;
 
-	public SampleGameState (GameActivity gameActivity) {
-		super(gameActivity);
+  private float cameraY;
 
-		SevenGE.input.addInputProcessor(this);
-		SevenGE.input.addGestureProcessor(this);
+  World world;
+  Body apple;
+  Entity appleEntity;
 
-		SevenGE.assetManager.loadAssets("sample.pkg");
+  PositionComponent positionComponent;
+  SpriteComponent spriteComponent;
 
-		rendererSystem = new RendererSystem();
-		cameraSystem = new CameraSystem();
-		animationSystem = new AnimationSystem();
-		scriptingSystem = new ScriptingSystem();
 
-		Random rng = new Random();
-		entities = new ArrayList<Entity>();
-		Entity cam = new Entity();
-		entities.add(cam);
-		for (int i = 0; i < 200; i++) {
-			Entity e = new Entity();
-			SpriteComponent cs = new SpriteComponent();
-			PositionComponent cp = new PositionComponent();
-			cs.scale = 1.0f;
-			float rnd = rng.nextFloat();
-			if (rnd < 0.5f) {
-				cs.textureRegion = (TextureRegion)SevenGE.assetManager.getAsset("meteorBrown_big1");
-				cs.scale = 1f;
-			} else if (rnd < 0.7f)
-				cs.textureRegion = (TextureRegion)SevenGE.assetManager.getAsset("meteorBrown_small2");
-			else if (rnd < 0.95f)
-				cs.textureRegion = (TextureRegion)SevenGE.assetManager.getAsset("meteorBrown_tiny2");
-			else {
-				cs.textureRegion = (TextureRegion)SevenGE.assetManager.getAsset("enemyRed1");
-				AnimationComponent ca = new AnimationComponent();
-				ca.frameList = new TextureRegion[] {(TextureRegion)SevenGE.assetManager.getAsset("enemyBlack1"),
-					(TextureRegion)SevenGE.assetManager.getAsset("enemyBlack2"),
-					(TextureRegion)SevenGE.assetManager.getAsset("enemyBlack3"),
-					(TextureRegion)SevenGE.assetManager.getAsset("enemyBlack4"),
-					(TextureRegion)SevenGE.assetManager.getAsset("enemyBlack5")};
-				ca.durations = new int[] {500, 1000, 2000, 234, 666};
-				ca.isPlaying = true;
-				e.add(ca, 4);
-			}
+  public SampleGameState(GameActivity gameActivity) {
+    super(gameActivity);
 
-			cp.rotation = rng.nextFloat() * 360.0f;
-			cp.x = rng.nextFloat() * 1000f;
-			cp.y = rng.nextFloat() * 1000f;
-			Matrix.setIdentityM(cp.scaleMatrix, 0);
-			Matrix.scaleM(cp.scaleMatrix, 0, cs.scale, cs.scale, 1.0f);
+    SevenGE.input.addInputProcessor(this);
+    SevenGE.input.addGestureProcessor(this);
 
-			Matrix.setIdentityM(cp.transform, 0);
-			Matrix.translateM(cp.transform, 0, cs.textureRegion.width / 2, cs.textureRegion.height / 2, 0f);
-			Matrix.rotateM(cp.transform, 0, cp.rotation, 0f, 0f, 1.0f);
-			Matrix.translateM(cp.transform, 0, cp.x, cp.y, 0f);
+    SevenGE.assetManager.loadAssets("sample.pkg");
 
-			e.add(cp, 1);
-			e.add(cs, 2);
+    rendererSystem = new RendererSystem();
+    cameraSystem = new CameraSystem();
+    scriptingSystem = new ScriptingSystem();
 
-			optimizedEntites[oec++] = cp;
-			optimizedEntites[oec++] = cs;
+    world = new World(new Vector2(0, -10), true);
+    
 
-			// entities.add(e);
-		}
+    entities = new ArrayList<Entity>();
+    Random rng = new Random();
 
-		// music = (Music)SevenGE.assetManager.getAsset("music1");
-		// music.setLooping(true);
-		// music.play();
+    // paseczek
+    Entity bar = new Entity();
+    Entity cam = new Entity();
 
-	}
+    spriteComponent = new SpriteComponent();
+    spriteComponent.scale = 1.0f;
+    spriteComponent.textureRegion = (TextureRegion) SevenGE.assetManager.getAsset("buttonRed");
+    positionComponent = new PositionComponent();
+    positionComponent.x = 0;
+    positionComponent.y = -400;
 
-	@Override
-	public void onSurfaceChange (int width, int height) {
-		Entity e = entities.get(0);
-		cc = new CameraComponent();
-		cc.height = height;
-		cc.width = width;
-		cc.scale = 0.7f;
-		PositionComponent cp = new PositionComponent();
-		cp.x = 0;
-		cp.y = 0;
-		Matrix.setLookAtM(cc.viewMatrix, 0, cp.x, cp.y, 1f, cp.x, cp.y, 0f, 0f, 1.0f, 0.0f);
-		Matrix.orthoM(cc.projectionMatrix, 0, -width / cc.scale / 2, width / cc.scale / 2, -height / cc.scale / 2, height
-			/ cc.scale / 2, 0f, 1f);
-		Matrix.multiplyMM(cc.viewProjectionMatrix, 0, cc.projectionMatrix, 0, cc.viewMatrix, 0);
-		Matrix.invertM(cc.invertedVPMatrix, 0, cc.viewProjectionMatrix, 0);
-		e.add(cp, 1);
-		e.add(cc, 8);
-		entities.add(e);
-	}
+    Matrix.setIdentityM(positionComponent.scaleMatrix, 0);
+    Matrix.scaleM(positionComponent.scaleMatrix, 0, spriteComponent.scale, spriteComponent.scale,
+        1.0f);
 
-	@Override
-	public void update () {
-		SevenGE.input.process();
+    Matrix.setIdentityM(positionComponent.transform, 0);
+    Matrix.translateM(positionComponent.transform, 0, spriteComponent.textureRegion.width / 2,
+        spriteComponent.textureRegion.height / 2, 0f);
+    Matrix.rotateM(positionComponent.transform, 0, positionComponent.rotation, 0f, 0f, 1.0f);
+    Matrix.translateM(positionComponent.transform, 0, positionComponent.x, positionComponent.y, 0f);
 
-		angle = (float)((angle + 0.05f) % (Math.PI * 2));
-		cameraX = (float)(Math.cos(angle) * 500);
-		cameraY = (float)(Math.sin(angle) * 200);
+    bar.add(positionComponent, 1);
+    bar.add(spriteComponent, 2);
+    
+    entities.add(cam);
 
-		// cc.px = cc.x;
-		// cc.py = cc.y;
-		// cc.x = cameraX;
-		// cc.y = cameraY;
+    optimizedEntites[oec++] = positionComponent;
+    optimizedEntites[oec++] = spriteComponent;
 
-		// animationSystem.process(entities);
+    // ground body
 
-		cameraSystem.process(entities);
-		if (changeme >= 30) {
-			changeme = 0;
-			scriptingSystem.process(entities);
-		}
-		changeme++;
-	}
+    BodyDef groundBodyDef = new BodyDef();
+    groundBodyDef.position.set(new Vector2(WORLD_TO_BOX * positionComponent.x, WORLD_TO_BOX * positionComponent.y));
+    Body groundBody = world.createBody(groundBodyDef);
+    PolygonShape groundBox = new PolygonShape();
+    groundBox.setAsBox(WORLD_TO_BOX * spriteComponent.textureRegion.width / 2,
+        WORLD_TO_BOX * spriteComponent.textureRegion.height / 2);
+    groundBody.createFixture(groundBox, 0.0f);
 
-	@Override
-	public void draw (float a, boolean updated) {
-		rendererSystem.process(optimizedEntites, cc, oec, a, updated);
-	}
+    // apple
 
-	@Override
-	public void dispose () {
+    appleEntity = new Entity();
+    positionComponent = new PositionComponent();
+    positionComponent.x = 0;
+    positionComponent.y = 0;
+    spriteComponent = new SpriteComponent();
+    spriteComponent.scale = 1.0f;
+    spriteComponent.textureRegion = (TextureRegion) SevenGE.assetManager.getAsset("enemyRed5");
 
-	}
 
-	@Override
-	public void pause () {
-		// music.pause();
+    Matrix.setIdentityM(positionComponent.scaleMatrix, 0);
+    Matrix.scaleM(positionComponent.scaleMatrix, 0, spriteComponent.scale, spriteComponent.scale,
+        1.0f);
 
-	}
+    Matrix.setIdentityM(positionComponent.transform, 0);
+    Matrix.translateM(positionComponent.transform, 0, spriteComponent.textureRegion.width / 2,
+        spriteComponent.textureRegion.height / 2, 0f);
+    Matrix.rotateM(positionComponent.transform, 0, positionComponent.rotation, 0f, 0f, 1.0f);
+    Matrix.translateM(positionComponent.transform, 0, positionComponent.x, positionComponent.y, 0f);
 
-	@Override
-	public void resume () {
-		// music.play();
+    appleEntity.add(positionComponent, 1);
+    appleEntity.add(spriteComponent, 2);
 
-	}
+    optimizedEntites[oec++] = positionComponent;
+    optimizedEntites[oec++] = spriteComponent;
 
-	@Override
-	public boolean onDoubleTap (MotionEvent me) {
-		Log.d(TAG, "onDoubleTap");
-		return false;
-	}
+    // dynamic body
 
-	@Override
-	public boolean onSingleTapConfirmed (MotionEvent arg0) {
-		Log.d(TAG, "onSingleTapConfirmed");
-		return false;
-	}
+    BodyDef bodyDef = new BodyDef();
+    bodyDef.type = BodyDef.BodyType.DynamicBody;
+    bodyDef.position.set(WORLD_TO_BOX * positionComponent.x,WORLD_TO_BOX * positionComponent.y);
+    apple = world.createBody(bodyDef);
 
-	@Override
-	public boolean onFling (MotionEvent arg0, MotionEvent arg1, float arg2, float arg3) {
-		Log.d(TAG, "onFling");
-		return false;
-	}
+    CircleShape dynamicCircle = new CircleShape();
+    dynamicCircle.setRadius(WORLD_TO_BOX * spriteComponent.textureRegion.height / 2);
+    FixtureDef fixtureDef = new FixtureDef();
+    fixtureDef.shape = dynamicCircle;
+    fixtureDef.density = 5.0f;
+    fixtureDef.friction = 0f;
+    fixtureDef.restitution = 0.5f;
+    apple.createFixture(fixtureDef);
+    
 
-	@Override
-	public void onLongPress (MotionEvent arg0) {
-		Log.d(TAG, "onLongPress");
 
-	}
+  }
 
-	@Override
-	public boolean onScroll (MotionEvent arg0, MotionEvent arg1, float arg2, float arg3) {
-		Log.d(TAG, "onScroll");
-		return false;
-	}
+  @Override
+  public void onSurfaceChange(int width, int height) {
+    Entity e = entities.get(0);
+    cc = new CameraComponent();
+    cc.height = height;
+    cc.width = width;
+    cc.scale = 0.7f;
+    PositionComponent cp = new PositionComponent();
+    cp.x = 0;
+    cp.y = 0;
+    Matrix.setLookAtM(cc.viewMatrix, 0, cp.x, cp.y, 1f, cp.x, cp.y, 0f, 0f, 1.0f, 0.0f);
+    Matrix.orthoM(cc.projectionMatrix, 0, -width / cc.scale / 2, width / cc.scale / 2, -height
+        / cc.scale / 2, height / cc.scale / 2, 0f, 1f);
+    Matrix.multiplyMM(cc.viewProjectionMatrix, 0, cc.projectionMatrix, 0, cc.viewMatrix, 0);
+    Matrix.invertM(cc.invertedVPMatrix, 0, cc.viewProjectionMatrix, 0);
+    e.add(cp, 1);
+    e.add(cc, 8);
+    entities.add(e);
+  }
 
-	@Override
-	public boolean touchDown (int x, int y, int pointer, int button) {
-		Log.d(TAG, "touchDown" + " x : " + x + " , y : " + y + " , pointerid : " + pointer);
-		return false;
-	}
+  @Override
+  public void update() {
+    SevenGE.input.process();
 
-	@Override
-	public boolean touchUp (int x, int y, int pointer, int button) {
-		Log.d(TAG, "touchUp" + " x : " + x + " , y : " + y + " , pointerid : " + pointer);
-		return false;
-	}
+    angle = (float) ((angle + 0.05f) % (Math.PI * 2));
+    cameraX = (float) (Math.cos(angle) * 500);
+    cameraY = (float) (Math.sin(angle) * 200);
 
-	@Override
-	public boolean touchMove (int x, int y, int pointer) {
-		Log.d(TAG, "touchMove" + " x : " + x + " , y : " + y + " , pointerid : " + pointer);
-		return false;
-	}
+    cameraSystem.process(entities);
+    if (changeme >= 30) {
+      changeme = 0;
+      scriptingSystem.process(entities);
+    }
+    changeme++;
+
+    world.step(BOX_STEP, BOX_VELOCITY_ITERATIONS, BOX_POSITION_ITERATIONS);
+
+     positionComponent.x = BOX_WORLD_TO * apple.getPosition().x;
+     positionComponent.y = BOX_WORLD_TO * apple.getPosition().y;
+
+    Matrix.setIdentityM(positionComponent.scaleMatrix, 0);
+    Matrix.scaleM(positionComponent.scaleMatrix, 0, spriteComponent.scale, spriteComponent.scale,
+        1.0f);
+
+    Matrix.setIdentityM(positionComponent.transform, 0);
+    Matrix.translateM(positionComponent.transform, 0, spriteComponent.textureRegion.width / 2,
+        spriteComponent.textureRegion.height / 2, 0f);
+    Matrix.rotateM(positionComponent.transform, 0, positionComponent.rotation, 0f, 0f, 1.0f);
+    Matrix.translateM(positionComponent.transform, 0, positionComponent.x, positionComponent.y, 0f);
+
+
+  }
+
+  @Override
+  public void draw(float a, boolean updated) {
+    rendererSystem.process(optimizedEntites, cc, oec, a, updated);
+  }
+
+  @Override
+  public void dispose() {
+
+  }
+
+  @Override
+  public void pause() {
+    // music.pause();
+
+  }
+
+  @Override
+  public void resume() {
+    // music.play();
+
+  }
+
+  @Override
+  public boolean onDoubleTap(MotionEvent me) {
+    Log.d(TAG, "onDoubleTap");
+    return false;
+  }
+
+  @Override
+  public boolean onSingleTapConfirmed(MotionEvent arg0) {
+    Log.d(TAG, "onSingleTapConfirmed");
+    return false;
+  }
+
+  @Override
+  public boolean onFling(MotionEvent arg0, MotionEvent arg1, float arg2, float arg3) {
+    Log.d(TAG, "onFling");
+    return false;
+  }
+
+  @Override
+  public void onLongPress(MotionEvent arg0) {
+    Log.d(TAG, "onLongPress");
+
+  }
+
+  @Override
+  public boolean onScroll(MotionEvent arg0, MotionEvent arg1, float arg2, float arg3) {
+    Log.d(TAG, "onScroll");
+    return false;
+  }
+
+  @Override
+  public boolean touchDown(int x, int y, int pointer, int button) {
+    Log.d(TAG, "touchDown" + " x : " + x + " , y : " + y + " , pointerid : " + pointer);
+    return false;
+  }
+
+  @Override
+  public boolean touchUp(int x, int y, int pointer, int button) {
+    Log.d(TAG, "touchUp" + " x : " + x + " , y : " + y + " , pointerid : " + pointer);
+    return false;
+  }
+
+  @Override
+  public boolean touchMove(int x, int y, int pointer) {
+    Log.d(TAG, "touchMove" + " x : " + x + " , y : " + y + " , pointerid : " + pointer);
+    return false;
+  }
 
 }
